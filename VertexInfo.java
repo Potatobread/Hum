97a00a4db0451c98885ec300c470ee9adb98c7bc
Potatobread <xalex1234@hotmail.com>
2019-06-17 17:30:15 -0300

    Create VertexInfo.java

---
+++ VertexInfo.java
@@ -0,0 +1,20 @@
+package Greedy;
+
+public class VertexInfo {
+	
+	int state;
+	int degree;
+	
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public int getDegree() {
+		return degree;
+	}
+	public void setDegree(int degree) {
+		this.degree = degree;
+	}
+}
