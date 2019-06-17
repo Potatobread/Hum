ed13df8bd66bf754272fa7f45acc2a200e8b34c5
Potatobread <xalex1234@hotmail.com>
2019-06-17 17:29:52 -0300

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,20 @@
+package Greedy;
+
+public class Edge {
+	
+	int first;
+	int second;
+	
+	public int getFirst() {
+		return first;
+	}
+	public void setFirst(int first) {
+		this.first = first;
+	}
+	public int getSecond() {
+		return second;
+	}
+	public void setSecond(int second) {
+		this.second = second;
+	}	
+}
